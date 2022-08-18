138e8d6aea946172cdd09e64fd6e9e68c7b1630e
Carolina Morales <carolina.cmorales3@gmail.com>
2022-08-18 17:19:49 -0500

    config heroku

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,5 +4,5 @@ export const regexEmail =
   /^(([^<>()\[\]\\.,;:\s@”]+(\.[^<>()\[\]\\.,;:\s@”]+)*)|(“.+”))@((\[[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}])|(([a-zA-Z\-0–9]+\.)+[a-zA-Z]{2,}))$/;
 
 export const request = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://compras-testing-job.herokuapp.com/api",
 });
