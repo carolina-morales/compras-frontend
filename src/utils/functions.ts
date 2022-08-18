4911cc18ea41ce9b678983dd6282269422dcf4ef
Carolina Morales <carolina.cmorales3@gmail.com>
2022-08-18 17:58:25 -0500

    fix

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,8 +1,9 @@
 import jwt_decode from "jwt-decode";
 import { IUser } from "./interfaces";
 
-export const getUserByToken = (): { user: IUser; exp: number; iat: number } => {
+export const getUserByToken = (): any => {
   const token = localStorage.getItem("token");
+  if (!token) return false;
   const user = jwt_decode(token || "") as {
     user: IUser;
     exp: number;
